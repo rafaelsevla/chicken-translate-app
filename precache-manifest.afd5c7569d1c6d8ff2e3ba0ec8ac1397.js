17c0699df5cf5b3921a52e1cd7bf4ad2575f27a5
Rafael Costa <rafaelsevla@gmail.com>
2019-09-08 01:21:29 -0300

    Updates

---
+++ precache-manifest.afd5c7569d1c6d8ff2e3ba0ec8ac1397.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "222de63f040c6c501d9c545c74c2b3ab",
+    "url": "/chicken-translate-app/index.html"
+  },
+  {
+    "revision": "3fbc13df01d136fd4bcd",
+    "url": "/chicken-translate-app/static/js/2.d8d6047c.chunk.js"
+  },
+  {
+    "revision": "15fe78ad863068daeb7e",
+    "url": "/chicken-translate-app/static/js/main.52a9b343.chunk.js"
+  },
+  {
+    "revision": "b93086259985eede17d6",
+    "url": "/chicken-translate-app/static/js/runtime~main.f02cb74d.js"
+  }
+]);
